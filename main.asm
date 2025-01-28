7a1ccb355e0a7901f0d211e58eea32e5be08b8b5
Dmitry Slobodchikov <d.slobodchikov@intraclear.com>
2025-01-28 14:38:11 +0200

    Realize continuous quantum increment

--- main.asm
+++ main.asm
@@ -7,47 +7,14 @@
 ; Author : Dmitry Slobodchikov
 ;
 
-; Fuses: 
+; The ATTiny85 fuses are set as follows: 
 ;   HIGH=0xdf
-;   LOW=0xe2 (8MHz)
 ;   LOW=0xe1 (16MHz), Current
+; To reduce power consumption set the low fuse register as follows:
+;   LOW=0xe2 (8MHz)
 
 .include "tn85def.inc"
-
-.equ F_CPU = 16384000 ; CPU frequency defined as 16 MHz
-
-; --- Frequently used register definitions 
-.def dClock   = R12
-.def dRate    = R13
-.def cntHL    = R14
-.def cntd     = R15
-.def tmp      = R16
-.def _EREG_   = R17
-.def txByte   = R18
-.def rxByte   = R19
-.def tcntL    = R20
-.def tcntH    = R21
-.def tmpL     = R22
-.def tmpH     = R23
-.def cntLL    = R24
-.def cntLH    = R25
-
-.equ LEDDDR   = DDRB
-.equ LEDPORT  = PORTB
-.equ LEDPIN   = PINB
-.equ LED0PIN  = PB1
-
-
-; --- Constant definitions
-#define QNT_THRESHOLD 240 ; it gives 16 prescaler for 1ms system quant
-#define SEC_THRESHOLD 1000
-
-
-; --- Event REGistry Flag Definitions
-.equ _QIF_    = 0       ; System Quant Interval Flag
-.equ _SIF_    = 1       ; Second Interval Flag
-.equ _SMF_    = 2       ; Sleep Mode Flag
-.equ _LBF_    = 3       ; LED Blink Flag
+.include "./inc/avrdef.inc"
 
 
 ; --- Set start address
